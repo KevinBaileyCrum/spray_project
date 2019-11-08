a54e8b467d929dee1b83ed62f604f0719f4fb10d
Kevin Crum <kevinbaileycrum@gmail.com>
2019-11-08 14:56:31 -0800

    clean

--- myApp/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-const axios = require('axios')
-const cheerio = require('cheerio')
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
