28c077b82e23091a57db2a8f1a86c1f1af41370d
Kevin Crum <kevinbaileycrum@gmail.com>
2019-12-10 10:08:35 -0800

    created tick object prototype

--- ticks.js
+++ ticks.js
@@ -3,9 +3,29 @@ const express = require('express')
 const router = express.Router()
 const apiKey = require('./apiKey')
 
+function TickObj(
+  userName = null,
+  routeName= null,
+  routeGrade = null,
+  date = null,
+  style  = null,
+  notes = null,
+  stars = null
+){
+  this.userName = userName
+  this.routeGrade = routeName
+  this.routeGrade = routeGrade
+  this.date = date
+  this.style = style
+  this.notes = notes
+  this.starts = stars
+}
+
+
 router.get('/', function(req, res, next) {
-  console.log('nodemon test')
-  console.log(apiKey.apiKey)
+  var tick = new TickObj()
+  tick.routeName = 'Midnight Lightning'
+  console.log(tick)
   res.send('ticks endpoit')
 })
 
