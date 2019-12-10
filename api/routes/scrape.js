84f519e306f52cdde1e03e8227ab91fa4f50a71a
Kevin Crum <kevinbaileycrum@gmail.com>
2019-12-09 17:18:42 -0800

    getTicks also call mp api to get route name for tick. only calling one though

--- api/routes/scrape.js
+++ api/routes/scrape.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const apiKey = require('./apiKey');
 const axios = require('axios');
 
-function getUserTick(user) {
+function getUser(user) {
   return axios.get('https://www.mountainproject.com/data/get-user?', {
     params: {
       userId: user,
@@ -11,20 +11,53 @@ function getUserTick(user) {
     }
   })
     .then((response) => {
-      return response.data.name;
+      // return response.data.name;
+      return response.data;
     })
+}
+
+function getTicks(user) {
+  return axios.get('https://www.mountainproject.com/data/get-ticks?', {
+    params: {
+      userId: user,
+      key: apiKey.apiKey,
+      startPos: 115
+    }
+  })
+    .then((response) => {
+      for (tick of response.data.ticks){
+        console.log(tick.routeId);
+        return axios.get('https://www.mountainproject.com/data/get-routes?', {
+          params: {
+            routeIds:  tick.routeId,
+            key: apiKey.apiKey
+          }
+        })
+          .then((response) => {
+            tick.routeId = response.data.routes[0].name
+            console.log(tick)
+            // return response.data.ticks
+          })
+        }
+      return response.data.ticks
+    })
+  // return response.tick
 }
 
 router.get('/', function(req, res, next) {
   userList = [
     '108543839', // kevin
-    '12117', // sirius
-    '200432149' // chris
+    // '12117', // sirius
+    // '200432149' // chris
   ];
-  var aResponse = userList.map(getUserTick);
-  Promise.all(aResponse)
+  // var aResponse = userList.map(getUser);
+  var bResponse = userList.map(getTicks);
+  Promise.all(bResponse)
     .then(responses=>{
-      console.log(responses);
+      // responses = JSON.stringify(responses, null, 2);
+      // console.log(bResponse);
+      console.log('resp ' + responses);
+
       res.render('scrape', {
         userList: userList,
         responses: responses
