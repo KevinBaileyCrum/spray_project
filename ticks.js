ce7b09a80e981643cebeadaa908199c7c1c79891
Kevin Crum <kevinbaileycrum@gmail.com>
2019-12-10 08:57:42 -0800

    portig over apiKey etc

--- ticks.js
+++ ticks.js
@@ -1,8 +1,11 @@
 // ticks.js
 const express = require('express')
 const router = express.Router()
+const apiKey = require('./apiKey')
 
 router.get('/', function(req, res, next) {
+  console.log('nodemon test')
+  console.log(apiKey.apiKey)
   res.send('ticks endpoit')
 })
 
