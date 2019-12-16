727227810f19c65552d7d057349a6d4c50eb0e40
Kevin Crum <kevinbaileycrum@gmail.com>
2019-12-16 14:37:37 -0800

    using async was able to query api and await for response before continuing

--- ticks.js
+++ ticks.js
@@ -1,32 +1,68 @@
 // ticks.js
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 const apiKey = require('./apiKey')
 
 function TickObj(
-  userName = null,
-  routeName= null,
-  routeGrade = null,
-  date = null,
-  style  = null,
-  notes = null,
-  stars = null
+    userName = null, // getUser api
+    routeId = null,  // tick api vvv
+    date = null,
+    style  = null,
+    notes = null,
+    stars = null,
+    routeName = null, // route api vvv
+    routeGrade = null
 ){
-  this.userName = userName
-  this.routeGrade = routeName
-  this.routeGrade = routeGrade
-  this.date = date
-  this.style = style
-  this.notes = notes
-  this.starts = stars
+    this.userName = userName
+    this.routeGrade = routeName
+    this.routeId = routeId
+    this.date = date
+    this.style = style
+    this.notes = notes
+    this.stars = stars
+    this.routeName = routeName
+    this.routeGrade = routeGrade
 }
 
+mpApiGetTicks = async (userId) => {
+    console.log(userId)
+    return await axios.get('https://www.mountainproject.com/data/get-ticks?', {
+        params: {
+            userId: userId,
+            key: apiKey.apiKey,
+            startPos: 115
+        }
+    })
+    .then((response) => {
+        console.log('axios here')
+        console.log(response.data)
+        return response.data.ticks
+    })
+}
 
-router.get('/', function(req, res, next) {
-  var tick = new TickObj()
-  tick.routeName = 'Midnight Lightning'
-  console.log(tick)
-  res.send('ticks endpoit')
+const getTicks = async (userId) => {
+    console.log(userId)
+    console.log('there')
+    const mpTicksRes = await mpApiGetTicks(userId)
+    if (mpTicksRes) {
+        console.log(`mpTicksRes ${mpTicksRes}`)
+    }
+}
+
+router.get('/', function(req, res) {
+
+    // assuming already have userID
+    // for friend in friend list
+    //   getTicks(listOfTickObjs)
+    //   for ticks in listOfTickObjs
+    //     getRoute info on tick
+
+    var friendList = ['108543839'] // kevin
+    var TickList = []
+    getTicks(friendList[0]).then(response => console.log('here'))
+    // mpApiGetTicks(friendList[0]).then((response) => console.log(response))
+
 })
 
 module.exports = router
