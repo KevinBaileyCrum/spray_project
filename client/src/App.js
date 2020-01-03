30d1db3c873d19b5095a9ee1c5f9445dd214da6c
Kevin Crum <kevinbaileycrum@gmail.com>
2020-01-02 23:11:03 -0800

    boxing css

--- client/src/App.js
+++ client/src/App.js
@@ -23,15 +23,19 @@ class TickList extends React.Component{
         return (
             <div className="tick-card-list">
                 { this.state.ticks.map(tick =>
-                <div className="tick-card" key={tick.tickId}>
-                    <div className="tick-card-userName">{tick.userName}</div>
-                    <div className="tick-card-routeId">{tick.routeId}</div>
-                    <div className="tick-card-date">{tick.date}</div>
-                    <div className="tick-card-style">{tick.style}</div>
-                    <div className="tick-card-notes">{tick.notes}</div>
-                    <div className="tick-card-stars">{tick.stars}</div>
-                    <div className="tick-card-routeName">{tick.routeName}</div>
-                    <div className="tick-card-routeGrade">{tick.routeGrade}</div>
+                <div className="tick-card-item" key={tick.tickId}>
+                    <div className="tick-card">
+                        <div className="tick-card-content">
+                            <div className="tick-card-userName">{tick.userName}</div>
+                            <div className="tick-card-routeId">{tick.routeId}</div>
+                            <div className="tick-card-date">{tick.date}</div>
+                            <div className="tick-card-style">{tick.style}</div>
+                            <div className="tick-card-notes">{tick.notes}</div>
+                            <div className="tick-card-stars">{tick.stars}</div>
+                            <div className="tick-card-routeName">{tick.routeName}</div>
+                            <div className="tick-card-routeGrade">{tick.routeGrade}</div>
+                        </div>
+                    </div>
                 </div>
                 )}
             </div>
